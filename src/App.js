0a1b3e4e75e00352af6faabf8f9e8c82bd9fb0e1
AnirudhkrishnaS <113175052+AnirudhkrishnaS@users.noreply.github.com>
2022-11-22 17:11:31 +0530

    OLX-clone user signUp and login done

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Signup from './Components/Signup/Signup'
+import Login from './Pages/Login'
+
+
+
+
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/signup'>
+        <Signup/>
+        </Route>
+        <Route path='/login'>
+        <Login/>
+        </Route>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FireBase/config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+import 'firebase/auth'
+import 'firebase/firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAIeysHwJSqzKdDjRynB700XJBQ9l39GRs",
+    authDomain: "olx-clone-90450.firebaseapp.com",
+    projectId: "olx-clone-90450",
+    storageBucket: "olx-clone-90450.appspot.com",
+    messagingSenderId: "135418477098",
+    appId: "1:135418477098:web:d03d6c51e0a9f592d22153",
+    measurementId: "G-DQ1099KT98"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
+
