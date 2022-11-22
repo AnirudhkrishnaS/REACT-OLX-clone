dfdc380f8db00219f302262d722d6bd5768d11e7
AnirudhkrishnaS <113175052+AnirudhkrishnaS@users.noreply.github.com>
2022-11-23 00:15:33 +0530

    Olx-Clone-UserName-Logout-set

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React,{useEffect,useContext} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Signup from './Components/Signup/Signup'
 import Login from './Pages/Login'
+import {AuthContext, FirebaseContext} from './store/Context'
 
 
 
@@ -10,7 +11,16 @@ import Login from './Pages/Login'
 import Home from './Pages/Home';
 
 function App() {
-  return (
+  const {setUser} = useContext(AuthContext)
+  const {firebase} = useContext(FirebaseContext)
+  useEffect(()=>{
+    firebase.auth().onAuthStateChanged((user)=>{
+      setUser(user);
+    })
+    
+
+  })
+return (
     <div>
       <Router>
         <Route exact path='/'>
